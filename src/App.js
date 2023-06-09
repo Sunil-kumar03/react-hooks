29d1101eee93efb12c76120b121f9c6f19432767
Sunil-kumar03 <challasunil2002@gmail.com>
2023-06-09 13:51:48 +0530

    updated useCallBack,useMemo,useRef hooks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Ex1 from './components/Ex1'
 import Ex2 from './components/Ex2'
 import Ex3 from './components/Ex3'
 import Ex4 from './components/Ex4'
+import Ex5 from './components/Ex5'
+import Ex6 from './components/Ex6'
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path={`/ex2`} element={<Ex2 itemsPerPage={10} />}></Route>
           <Route path={`/ex3`} element={<Ex3 itemsPerPage={5}/>}></Route>
           <Route path={`/ex4`} element={<Ex4/>}></Route>
+          <Route path={`/ex5`} element={<Ex5/>}></Route>
+          <Route path={`/ex6`} element={<Ex6/>}></Route>
           <Route path={`/*`} element={<Ex1/>}></Route>
        </Routes>
     </BrowserRouter>
--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React,{useState,useCallback} from 'react'
+import Age from './sub/Age'
+import AgeInfo from './sub/AgeInfo'
+
+//useCallBack => will return memorized value of callback
+//useMemo => will return memorized function of callback
 
 function Ex4() {
+  const [age,setAge]=useState(10)
+
+  const incrementAge=  ()=>{
+    setAge(age+1)
+  }
+  const userAge =`David age is ${age} years`
+
+  //useCallback will return a memorized version of the callback that only changes if one of the input has changed
+
+  const updateAge=useCallback(()=>{
+    return userAge
+  },[age])
+
   return (
     <div className='container'>
       <div className="row">
         <div className="col-md-12 text-center">
-            <h3 className="display-3 text-success">Ex4</h3>
+            <h3 className="display-3 text-success">useCallBack</h3>
         </div>
       </div>
+      <Age age={age} ageHandler={incrementAge}/>
+      <AgeInfo ageInfo={updateAge} />
     </div>
   )
 }
